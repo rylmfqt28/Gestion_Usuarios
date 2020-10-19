90d864580ad80e488591f1c1e3ad3957a29c301c
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-10-18 23:00:16 -0400

    Se creo la estructura del proyecto

---
+++ src/main/java/com/thejuniors/gestionusuarios/GestionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.thejuniors.gestionusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsuariosApplication.class, args);
+	}
+
+}

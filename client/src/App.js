2dd88bb2fb3cd45849168efe6a5e016be17b3ada
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-21 09:42:53 -0400

    estructura basica del login

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-         
-        </a>
-      </header>
-      <FormCrearTipoUsuario></FormCrearTipoUsuario>
-    </div>
+    <div className="containerPrincipal">
+               <div className="containerSecundario">
+                    <div className="from-group">
+                        <label>
+                            Usuario:
+                        </label>
+                        <br/>
+                        <input
+                            type="text"
+                            className="from-control"    
+                        />
+                        <br/>
+                        <label>Contrasena: </label>
+                        <br/>
+                        <input
+                            type="password"
+                            className="from-control" 
+                        />
+                        <br/>
+                        <button className="btn btn-primay">Iniciar sesion</button>    
+                        
+                    
+
+                    </div>
+               </div>
+   
+           </div>    
 
   );
 }

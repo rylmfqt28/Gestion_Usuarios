7b9a5dc896421d1116461647eae58ecd5f833d09
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-21 12:41:40 -0400

    interface login con componentes, queda por definir colores

--- client/src/App.js
+++ client/src/App.js
@@ -7,45 +7,76 @@ import './App.css';
 
 function App() {
   return (
-    <div className="containerPrincipal">
-               <div className="containerSecundario">
-                    <div className="from-group">
-                        <label>
-                          <b>
-                          Inicio de seción
+    <div>
+      <div className="barraNav">
+        <nav className="navbar navbar-light bg-light justify-content-between">
+          <a className="navbar-brand">Empresa</a>
+          <form className="form-inline">
+
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">REGISTARSE</button>
+          </form>
+        </nav>
+      </div>
+
+
+      <div className="containerPrincipal">
+
+
+
+        <div className="containerSecundario">
+          <div className="from-group">
+            <label>
+              <b>
+                Inicio de seción
                         </b>
-                            
-                        </label>
-                        <br/>
-                        <br/>
 
-                        <label>
-                            Usuario:
-                        </label>
-                        <br/>
-                        <input
-                            type="text"
-                            className="from-control" 
-                            placeholder="Ingrese su usuario"   
-                        />
-                        <br/>
-                        <label>Contraseña: </label>
-                        <br/>
-                        <input
-                            type="password"
-                            className="from-control" 
-                            placeholder="Ingrese su contraseña"
-                        />
-                        <br/>
-                        <br/>
-                        <button className="btn btn-primary">Iniciar sesion</button>    
-                        
-                    
+            </label>
+            <br />
+            <br />
 
-                    </div>
-               </div>
-   
-           </div>    
+            <label>
+              Usuario:
+                        </label>
+            <br />
+            <input
+              type="text"
+              className="from-control"
+              placeholder="Ingrese su usuario"
+            />
+            <br />
+            <label>Contraseña: </label>
+            <br />
+            <input
+              type="password"
+              className="from-control"
+              placeholder="Ingrese su contraseña"
+            />
+            <br />
+            <br />
+            <div className="checkbox">
+              <label>
+                <input
+                  type="checkbox" value=""
+                />
+                            Recordar cuenta
+                          </label>
+
+            </div>
+
+            <button className="btn btn-primary">Iniciar sesion</button>
+
+            <div className="enlaceRegister">
+              <a href="">
+                ¿Aún no tienes una cuenta? registrate aqui
+                          </a>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
 
   );
 }

755c7e4a097a0d1c49f42339fe7115d521353ecc
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-21 20:12:38 -0400

    estilos y colores al Login

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <div>
       <div className="barraNav">
-        <nav className="navbar navbar-light bg-light justify-content-between">
+        <nav className="navbar navbar-light justify-content-between">
           <a className="navbar-brand">Empresa</a>
           <form className="form-inline">
 
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">REGISTARSE</button>
+            <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">REGISTARSE</button>
           </form>
         </nav>
       </div>
@@ -22,60 +22,61 @@ function App() {
       <div className="containerPrincipal">
 
 
-
-        <div className="containerSecundario">
-          <div className="from-group">
-            <label>
-              <b>
-                Inicio de seción
+        <from>
+          <div className="containerSecundario">
+            <div className="from-group">
+              <label className="title-inicio">
+                <b>
+                  Inicio de seción
                         </b>
 
-            </label>
-            <br />
-            <br />
+              </label>
+              <br />
+              <br />
 
-            <label>
-              Usuario:
-                        </label>
-            <br />
-            <input
-              type="text"
-              className="from-control"
-              placeholder="Ingrese su usuario"
-            />
-            <br />
-            <label>Contraseña: </label>
-            <br />
-            <input
-              type="password"
-              className="from-control"
-              placeholder="Ingrese su contraseña"
-            />
-            <br />
-            <br />
-            <div className="checkbox">
               <label>
+                Usuario:
+                        </label>
+              <br />
+              <input
+                type="text"
+                className="from-control"
+                placeholder="Ingrese su usuario"
+              />
+              <br />
+              <label>Contraseña: </label>
+              <br />
+              <input
+                type="password"
+                className="from-control"
+                placeholder="Ingrese su contraseña"
+              />
+              <br />
+              <br />
+              <div className="checkbox">
+
                 <input
                   type="checkbox" value=""
-                />
-                            Recordar cuenta
+                />  <label>
+                  Recordar cuenta
                           </label>
 
-            </div>
+              </div>
 
-            <button className="btn btn-primary">Iniciar sesion</button>
+              <button className="btn btn-primary">Iniciar sesion</button>
 
-            <div className="enlaceRegister">
-              <a href="">
-                ¿Aún no tienes una cuenta? registrate aqui
+              <div className="enlaceRegister">
+                <a href="">
+                  ¿Aún no tienes una cuenta? registrate aqui
                           </a>
 
+              </div>
+
             </div>
-
           </div>
-        </div>
-
+        </from>
       </div>
+
     </div>
 
   );

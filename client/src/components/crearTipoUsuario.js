9a98ba842535b434b6d85d2c03dd4233bd31d4ab
Ezequiel Rene Lafuente Machado <ezqui016@gmail.com>
2020-10-22 21:58:10 -0400

    Implementacion del diseño y validacion del formulario crear tipo de usuario

--- client/src/components/crearTipoUsuario.js
+++ client/src/components/crearTipoUsuario.js
@@ -1,41 +1,101 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import '../crearTipoUsuario.css';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
 
-class FormCrearTipoUsuario extends  React.Component{
-    constructor(args){
-        super(args)
-        this.state ={
-            users:[
+const CrearTipoUsuario = () => {
 
-            ]
-        }
-
+    const { register, errors, handleSubmit } = useForm();
+    const onSubmit = (data, e) => {
+        console.log(data)
+        e.target.reset()
     }
 
-    render(){
-      
-        return(
-            
-            <div className="crearTipoUsuario"> 
-                    
-                <h1 html>Crear Nuevo Tipo de Usuario</h1>
-                <input clasName ="createusertype" id="createusertype" maxLength="20" type="text"></input>
-                <br></br>
-                
-                
-                <h1>Descripcion</h1>
-                <div className="containerArea">
-                    <textarea className="textArea" rows="10" cols="40">
 
-                    </textarea>
-                </div>
+    return (
+
+        <Fragment>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>    
+            </nav>
+
+        
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <h2>Crear tipo de Usuario</h2>
                 <div>
-                <Button variant="outlined" sized="large" color="primary" ></Button>
-                </div>
-            </div>
-        )     
+                    <input
+                        name="crearTipo"
+                        className="campoTipo"
+                        ref={
+                            register({
+                                required: {
+                                    value: true, message: 'Ingrese Tipo de Usuario'
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message: 'No más de 20 carácteres!'
+                                },
+                                minLength: {
+                                    value: 5,
+                                    message: 'Mínimo 5 carácteres'
+                                }
+                            })
 
-    }  
+
+                        }
+                        placeholder="Ingresar tipo de u suario"
+
+                    />
+                    <span className="text-danger text-small d-block mb-2">
+                        {errors?.crearTipo?.message}
+                    </span>
+
+                </div>
+
+                <div>
+
+                    <textarea
+                        name="descripcion"
+                        className="textArea"
+                        ref={
+                            register({
+                                required: {
+                                    value: true, message: 'Ingrese descripción'
+                                },
+                                maxLength: {
+                                    value: 250,
+                                    message: 'No más de 250 carácteres'
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: 'Mínimo 2 carácteres'
+                                }
+                            })
+
+
+                        }
+                        placeholder="Ingrese la Descripción Aquí"
+                    />
+                    <span className="text-danger text-small d-block mb-2">
+                        {errors?.descripcion?.message}
+                    </span>
+                    <br></br>
+                    <div className="botones">
+                        <button className="btn btn-primary  pull-right btn-lg" >Cancelar</button>
+                        <button className="btn btn-primary pull-left btn-lg" >Crear</button>   
+                    </div>
+                    
+
+                </div>
+
+            </form>
+        </Fragment>
+    );
 }
-export default FormCrearTipoUsuario;
+
+
+export default CrearTipoUsuario;

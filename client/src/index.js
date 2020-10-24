459f1454ddc89c47e554a57a8d8454b705ba8f74
henry001m <henrymontano700@gmail.com>
2020-10-23 20:41:45 -0400

    implementacion del evento del boton ver usuario de la historia solicitudes con la ventana emergente

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );

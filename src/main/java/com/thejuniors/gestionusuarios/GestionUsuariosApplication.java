caeb2caab12d9f9b5372fdbcbcb2fdced16efbd5
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-24 17:07:36 -0400

    se conecto a la base de datos

--- src/main/java/com/thejuniors/gestionusuarios/GestionUsuariosApplication.java
+++ src/main/java/com/thejuniors/gestionusuarios/GestionUsuariosApplication.java
@@ -8,6 +8,8 @@ public class GestionUsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionUsuariosApplication.class, args);
+
+		System.out.println("Probando si corre java.............");
 	}
 
 }

78956ec33dc236e0500e9789bdf30258983c0be3
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-10-24 18:30:47 -0400

    Se creo la estructura de paquetes para el backend y se agrego las dependencias a mysql y jdbc

---
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/HolaController.java
@@ -0,0 +1,12 @@
+package com.thejuniors.gestionusuarios.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+    @RequestMapping("/")
+    public String hola() {
+        return "Que miras curioso!!";
+    }
+}

ff469d72d851d4af1dac4852666e3a8355dbc7e7
Ezequiel Rene Lafuente Machado <ezqui016@gmail.com>
2020-10-29 22:00:10 -0400

    Resolucion de conflictos

--- src/main/java/com/thejuniors/gestionusuarios/controllers/UsuarioDao.java
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/UsuarioDao.java
@@ -1,10 +1,10 @@
 package com.thejuniors.gestionusuarios.controllers;
 
-import java.util.Optional;
+
 
 import com.thejuniors.gestionusuarios.model.Usuario;
 
-import org.hibernate.mapping.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioDao  extends CrudRepository<Usuario,String>{

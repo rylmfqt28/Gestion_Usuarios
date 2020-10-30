29423ea4ebf832944d4ffee7b0e4ca3758683f86
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-29 23:38:34 -0400

    se actualizo rama develop con los ultimos cambios

--- src/main/java/com/thejuniors/gestionusuarios/services/SolicitudeService.java
+++ src/main/java/com/thejuniors/gestionusuarios/services/SolicitudeService.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.thejuniors.gestionusuarios.model.UsuarioSolicitud;
+//import com.thejuniors.gestionusuarios.model.UsuarioSolicitud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;

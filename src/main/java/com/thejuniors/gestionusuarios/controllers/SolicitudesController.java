4423e117fa82e69332e418fa99b746677c2787ed
ronald quispe espinoza <r24.espinoza@gmail.com>
2020-10-30 23:24:14 -0400

    se crearon los controladores para lista de solicitud

--- src/main/java/com/thejuniors/gestionusuarios/controllers/SolicitudesController.java
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/SolicitudesController.java
@@ -1,5 +1,29 @@
 package com.thejuniors.gestionusuarios.controllers;
+
+import com.google.gson.Gson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.thejuniors.gestionusuarios.services.SolicitudeService;
+import com.thejuniors.gestionusuarios.model.UsuarioSolicitud;
+
+import java.util.List;
+
+
 //LLamar y dar rutas a las solicitudes para SolicitudesService
+@RestController
 public class SolicitudesController {
+    @Autowired
+    SolicitudeService loginservice;
+
+    @Autowired
+    Gson gson;
+
+    @GetMapping(value="/api/listaSolicitud/{tipoUsuarioNombre}/{nombreEstado}", produces={"application/json"})
+    public List<UsuarioSolicitud> login(@PathVariable("tipoUsuarioNombre") String tipoUsuarioNombre, @PathVariable("nombreEstado") String nombreEstado) {
+        return loginservice.enlistarSolicitudes(tipoUsuarioNombre, nombreEstado);
+    }
     
 }

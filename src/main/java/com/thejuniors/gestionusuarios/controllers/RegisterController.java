ef9b162d5fbfcbbdef99714a65fb4ee137034af8
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-11-10 16:22:25 -0400

    Se agrego la solicitud GET en crossorigin para el controlador de registro

--- src/main/java/com/thejuniors/gestionusuarios/controllers/RegisterController.java
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/RegisterController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@CrossOrigin(origins = "*", methods = {RequestMethod.POST})
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET})
 public class RegisterController {
     
     @Autowired

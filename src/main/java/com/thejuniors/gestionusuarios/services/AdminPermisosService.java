ea1080e360c5b83977aeb3921a1289b307306ad6
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-11-11 17:43:03 -0400

    Se creo el query para obtener la lista de los permisos existentes

---
+++ src/main/java/com/thejuniors/gestionusuarios/services/AdminPermisosService.java
@@ -0,0 +1,48 @@
+package com.thejuniors.gestionusuarios.services;
+
+import com.thejuniors.gestionusuarios.model.Permisos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminPermisosService {
+    
+    @Autowired		
+    private JdbcTemplate jdbcTemplate;
+
+    // Lista todos los permisos
+    public List<Permisos> listaPermisos(){
+        List<Permisos> permisos = jdbcTemplate.query(new PreparedStatementCreator(){
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement("SELECT permisosId, nombrePermiso, permisoDescripcion FROM Permisos");
+                return ps;
+            }
+        }, new ResultSetExtractor <List<Permisos>>(){
+            @Override
+            public List<Permisos> extractData(ResultSet rs) throws SQLException {
+                List<Permisos> permisosLista = new ArrayList<>();
+                while (rs.next()){
+                    Permisos permisos = new Permisos(rs.getInt("permisoId"), rs.getString("nombrePermiso"), rs.getString("permisoDescripcion"));
+                    permisosLista.add(permisos);
+                } 
+                return permisosLista;
+            }
+        });
+        if(permisos != null){
+            return permisos;
+        }else{
+            return null;
+        }
+    }
+
+}

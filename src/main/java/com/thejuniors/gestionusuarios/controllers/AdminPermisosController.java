8c95c0f47b10720563c76fe46e38d403c67d21c6
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-11-12 16:12:58 -0400

    Se implemento la entidad UsuarioPermisos

--- src/main/java/com/thejuniors/gestionusuarios/controllers/AdminPermisosController.java
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/AdminPermisosController.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT})
@@ -34,5 +35,8 @@ public class AdminPermisosController {
     public List<Permisos> listarPermisosNoAsignadosUsuario(@PathVariable("tipoUsuarioNombre") String tipoUsuarioNombre){
         return adminPermiso.listaPermisosNoUsaurio(tipoUsuarioNombre.replace("+", " "));
     }
+
+    @PostMapping(value = "/api/asignarPermiso", consumes={"application/json"})
+    public void agregarPermiso(){}
 
 }

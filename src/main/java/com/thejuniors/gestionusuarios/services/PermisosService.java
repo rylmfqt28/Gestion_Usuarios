d1313b6bcef7c37ba23e105a4119f98c816463a8
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-11-13 20:18:43 -0400

    Consulta para devolver los datos de un tipo de permiso implementada

---
+++ src/main/java/com/thejuniors/gestionusuarios/services/PermisosService.java
@@ -0,0 +1,49 @@
+package com.thejuniors.gestionusuarios.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.thejuniors.gestionusuarios.model.Permisos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PermisosService {
+    
+    @Autowired		
+    private JdbcTemplate jdbcTemplate;
+
+    // Verifica si existe el permiso
+    public Permisos buscarUsuario(String nombrePermiso){
+        Permisos permiso = jdbcTemplate.query(new PreparedStatementCreator(){
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement("SELECT * FROM Permisos WHERE nombrePermiso=?");
+                ps.setString(1, nombrePermiso);
+                return ps;
+            }
+        }, new ResultSetExtractor<Permisos>(){
+            @Override
+            public Permisos extractData(ResultSet rs) throws SQLException{
+                if (rs.next()){
+                    Permisos permiso = new Permisos(rs.getInt("permisoId"), rs.getString("nombrePermiso"), rs.getString("permisoDescripcion"));
+                    return permiso;
+                } else{
+                    return null;
+                }
+            }
+        });
+        if(permiso != null){
+            return permiso;
+        }else{
+            return null;
+        }
+    }
+
+}

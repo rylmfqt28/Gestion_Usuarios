09ec4b28db5ff49ce83c314e41faa3e1c4193e96
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-11-16 16:36:21 -0400

    Se creo ruta para acceder al query de insertar nuevo permiso

--- src/main/java/com/thejuniors/gestionusuarios/controllers/PermisosController.java
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/PermisosController.java
@@ -1,12 +1,15 @@
 package com.thejuniors.gestionusuarios.controllers;
 
 import com.google.gson.Gson;
+import com.thejuniors.gestionusuarios.model.EditPermiso;
 import com.thejuniors.gestionusuarios.services.PermisosService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,6 +27,12 @@ public class PermisosController {
     @GetMapping(value="/api/permiso/{nombrePermiso}", produces={"application/json"})
     public String login(@PathVariable("nombrePermiso") String nombrePermiso){
         return gson.toJson(permisos.buscarPermiso(nombrePermiso.replace("+", " ")));
+    }
+
+    // Inserta un nuevo permiso en la tabla PErmisos de la BD
+    @PostMapping(value = "/api/nuevoPermiso", consumes={"application/json"})
+    public void insertarPermiso(@RequestBody EditPermiso edtPermiso){
+        permisos.nuevoPermiso(edtPermiso);
     }
 
 }

99e41ca8fe09bde0a7302672201915dc0bde22ff
Ezequiel Rene Lafuente Machado <ezqui016@gmail.com>
2020-11-16 20:03:22 -0400

    Se alinearon los espacios en campo Genero

--- client/src/components/createAccount/newAccount.jsx
+++ client/src/components/createAccount/newAccount.jsx
@@ -633,7 +633,7 @@ capturarDatosModal = (motivo) => {
                   </div>
                   <div className="form-group">
                     <label className="form-check-label">
-                      <b>Género: </b>
+                      <b className="genero">Género: </b>
                     </label>
                     <div className="form-check form-check-inline">
                       <input
@@ -646,10 +646,11 @@ capturarDatosModal = (motivo) => {
                         required
 
                       />
-                      <label htmlFor="male" className="form-check-label"> Masculino</label>
 
+                      <label htmlFor="male" className="form-check-label"> Masculino</label>
+                      
                       <input
-                        className="form-check-input"
+                        className="form-check-inputM"
                         type="radio"
                         id="female"
                         name="gender"
@@ -657,12 +658,13 @@ capturarDatosModal = (motivo) => {
                         value="Femenino"
                         required
                       />
-                      <label htmlFor="female" className="form-check-label"> Femenino</label>
+                      <br></br>
+                      <label htmlFor="female" className="form-check-label">Femenino</label>
 
                       <input
                         type="radio"
                         id="other"
-                        className="form-check-input"
+                        className="form-check-inputF"
                         name="gender"
                         onChange={(e) => this.handleOnChange(e)}
                         value="Otro"

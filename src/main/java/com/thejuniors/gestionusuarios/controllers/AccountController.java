983f5d94cb1f0229963a60d786b6d4ada8105ce4
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-12-01 07:31:54 -0400

    Implementacion de controlador para obtener datos de una cuenta

---
+++ src/main/java/com/thejuniors/gestionusuarios/controllers/AccountController.java
@@ -0,0 +1,27 @@
+package com.thejuniors.gestionusuarios.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.thejuniors.gestionusuarios.services.AccountService;
+import com.thejuniors.gestionusuarios.model.Account;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT})
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping(value="/api/accountData/{CI}", produces={"application/json"})
+    public List<Account> getAccountData(@PathVariable("CI") String CI){
+        return accountService.accountData(CI);
+    }
+
+}

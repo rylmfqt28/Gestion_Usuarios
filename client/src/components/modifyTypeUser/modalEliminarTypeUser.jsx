eb1cbc3ee2e0f9a4b46936e5e16691973d4a1696
AlfredoAruquipa <leebyungchulk@gmail.com>
2020-12-01 21:34:42 -0600

    Se implemento la Funcionalidad del boton cancelar en eliminar TipoUsuario

--- client/src/components/modifyTypeUser/modalEliminarTypeUser.jsx
+++ client/src/components/modifyTypeUser/modalEliminarTypeUser.jsx
@@ -49,7 +49,7 @@ class ModalEliminarPermiso extends Component{
                            <h7>Decea eliminar el Usuario: "{this.props.nombrePermiso}" ?</h7>
                         </div>
                         <div className="modal-footer justify-content-center" >
-                            <button type="button" className="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                            <button type="button" className="btn btn-outline-info" data-dismiss="modal">Cancelar</button>
                             <button type="button" className="btn btn-outline-info"  onClick={this.handleErase}>Aceptar</button>
                         </div>
                     </div>

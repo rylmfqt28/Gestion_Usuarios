b6fa8cb0bc1289f3eb09bda3eaf7d4ce41d6cf8c
Rylmar Quispe <rylm.fqt28@gmail.com>
2020-12-05 23:42:23 -0400

    Implementacion de constructor y metodos getters y setters para ModifAccount

---
+++ src/main/java/com/thejuniors/gestionusuarios/model/ModifAccount.java
@@ -0,0 +1,110 @@
+package com.thejuniors.gestionusuarios.model;
+
+public class ModifAccount {
+
+    private String usuarioNombre;
+    private String usuarioApellido;
+    private String CI;
+    private Integer paisID;
+    private Integer ciudadID;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    private String nombreUsuario;
+    private String password;
+
+    public ModifAccount(String usuarioNombre, String usuarioApellido, String CI, Integer paisID, Integer ciudadID,
+            String direccion, String correo, String telefono, String nombreUsuario, String password) {
+        this.usuarioNombre = usuarioNombre;
+        this.usuarioApellido = usuarioApellido;
+        this.CI = CI;
+        this.paisID = paisID;
+        this.ciudadID = ciudadID;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+
+    public String getUsuarioNombre() {
+        return usuarioNombre;
+    }
+
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
+    }
+
+    public String getUsuarioApellido() {
+        return usuarioApellido;
+    }
+
+    public void setUsuarioApellido(String usuarioApellido) {
+        this.usuarioApellido = usuarioApellido;
+    }
+
+    public String getCI() {
+        return CI;
+    }
+
+    public void setCI(String CI) {
+        this.CI = CI;
+    }
+
+    public Integer getPaisID() {
+        return paisID;
+    }
+
+    public void setPaisID(Integer paisID) {
+        this.paisID = paisID;
+    }
+
+    public Integer getCiudadID() {
+        return ciudadID;
+    }
+
+    public void setCiudadID(Integer ciudadID) {
+        this.ciudadID = ciudadID;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

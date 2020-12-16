64480c37f4e3cae3d16ae1ba26b42e58f783c19d
henry001m <henrymontano700@gmail.com>
2020-12-16 19:35:30 -0400

    metodo de alert success

--- client/src/components/alert/Alerta.js
+++ client/src/components/alert/Alerta.js
@@ -23,12 +23,31 @@ class Alerta{
     });
   }
 
-  Alertadanger(alerta){
+  AlertaDanger(alerta){
     store.addNotification({
       id: 'hola',
       title: "Alert!!!",
       message: alerta,
       type: "danger",
+      insert: "top",
+      container: "top-left",
+      animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied
+      animationOut: ["animated", "fadeOut"],
+      dismiss: {
+        duration: 5000,
+        onScreen: true,
+        pauseOnHover: true,
+        showIcon: true
+      },
+    });
+  }
+
+  AlertaSuccess(alerta){
+    store.addNotification({
+      id: 'hola',
+      title: "Alert!!!",
+      message: alerta,
+      type: "success",
       insert: "top",
       container: "top-left",
       animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied

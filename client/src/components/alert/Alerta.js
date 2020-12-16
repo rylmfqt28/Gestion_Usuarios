522e2af658a09ac91a73ad7f3bdf3a5c2c3de2f5
henry001m <henrymontano700@gmail.com>
2020-12-16 19:34:43 -0400

    metodo de alert danger

--- client/src/components/alert/Alerta.js
+++ client/src/components/alert/Alerta.js
@@ -22,6 +22,25 @@ class Alerta{
       },
     });
   }
+
+  Alertadanger(alerta){
+    store.addNotification({
+      id: 'hola',
+      title: "Alert!!!",
+      message: alerta,
+      type: "danger",
+      insert: "top",
+      container: "top-left",
+      animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied
+      animationOut: ["animated", "fadeOut"],
+      dismiss: {
+        duration: 5000,
+        onScreen: true,
+        pauseOnHover: true,
+        showIcon: true
+      },
+    });
+  }
 }
 
 export default new Alerta();
